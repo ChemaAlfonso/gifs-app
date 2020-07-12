5b4e604f72c3579a663a0ac407f29f73563b0b9b
Chema Alfonso <jose.manuelchema111@gmail.com>
2020-07-12 14:07:21 +0200

    docs added

---
+++ docs/precache-manifest.556f6e973ef4f060e56b11fd69945100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926b5f4a7f04e4c943548ef2680e5360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c58306d32cf3f7c453c",
+    "url": "/static/css/main.36475e5c.chunk.css"
+  },
+  {
+    "revision": "12e3c73b787687a45d89",
+    "url": "/static/js/2.4f3b7ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f3b7ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c58306d32cf3f7c453c",
+    "url": "/static/js/main.802c9d2f.chunk.js"
+  },
+  {
+    "revision": "85b102bb13c4e971f478",
+    "url": "/static/js/runtime-main.a6bbda2f.js"
+  }
+]);
